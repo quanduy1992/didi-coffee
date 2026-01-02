d0481e4b19fe90b1925965724d1fcf8f5f86568b
Di Di Coffee <lerepipo6@gmail.com>
2026-01-02 12:41:13 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // File này lưu trữ các cấu hình dùng chung cho toàn bộ hệ thống Di Di Coffee
 const CONFIG = {
     // Dán link Web App của bạn vào đây một lần duy nhất
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxvSEdPoKd9HvBQGACqmTLSFqfBdNNyyD95Zf5mG--Z1wTD4Ah-UmKuiO_8srXba53MLg/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzuZ6dibrcB3h6cto8gvr5JDeSk8odFtP9ljCmQZXlLHhkEWVf0VoXZtZKUp7_Eilt7cQ/exec',
     
     // Bạn có thể thêm các cấu hình khác sau này tại đây
     REDEEM_POINTS: 10,
