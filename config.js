1a205689c3badf5d941fb916d50273899e5d84fa
Di Di Coffee <lerepipo6@gmail.com>
2026-01-17 20:15:58 +0700

    Update SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // File này lưu trữ các cấu hình dùng chung cho toàn bộ hệ thống Di Di Coffee
 const CONFIG = {
     // Dán link Web App của bạn vào đây một lần duy nhất
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyAKbnjH1q5nzmMjREW69fsnkdcrOKW0wn-Mfsc0NpS7u4yyyu64RZQTQGkWzUVmkkhdQ/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzQdq4v3v01Hl1bvX6YY6PjX3HhXZT829Nl_PvVOXACU4NpRzf1Q-2FiEHd4pr9T2j4LQ/exec',
     
     // Bạn có thể thêm các cấu hình khác sau này tại đây
     REDEEM_POINTS: 15,
@@ -11,6 +11,7 @@ const CONFIG = {
     LEVEL_2: 50, // Ví dụ: mốc 2 là 20 điểm
     SHOP_NAME: 'Di Di Coffee'
 };
+
 
 
 
