8c55b9e010de175353e434a68eca4128284f8855
Di Di Coffee <lerepipo6@gmail.com>
2026-01-17 12:14:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,10 @@ const CONFIG = {
     REDEEM_POINTS: 15,
     SPIN_POINTS: 200,
     POINTS_PER_CODE: 1,
+    CONFIG.LEVEL_1: 10, // Ví dụ: mốc 1 là 10 điểm
+    CONFIG.LEVEL_2: 20, // Ví dụ: mốc 2 là 20 điểm
     SHOP_NAME: 'Di Di Coffee'
 };
+
 
 
