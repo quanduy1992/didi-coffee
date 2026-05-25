a385191efb554878166cb5c86fe0ad300e3b21e0
Di Di Coffee <lerepipo6@gmail.com>
2026-05-25 19:44:22 +0700

    Update SCRIPT_URL in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // File này lưu trữ các cấu hình dùng chung cho toàn bộ hệ thống Di Di Coffee
 const CONFIG = {
     // Dán link Web App của bạn vào đây một lần duy nhất Di Di Coffee Nghĩ Lễ: 30/04 - 02/05
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyOusb42QCYF1zwJdmgYvB2THY26qCWAdmvChWw6TrLLgZlalHGykjht9TmuBvQcda7Zw/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxb4iHx0gPFVeI4eYq2jm2Xs4bgPsbuxcvxG7XSpYOfzU-XROhdf8ZZXcWly_XRFLEFVQ/exec',
     
     // Bạn có thể thêm các cấu hình khác sau này tại đây
     REDEEM_POINTS: 15,
